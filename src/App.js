c6751c01c0d70777a9dc5035f4c7fac576735667
ryuji <ryu.seven.h7420@gmail.com>
2020-05-09 10:40:41 +0900

    why jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function App() {
-  return <div><p>Hello World!</p></div>
+class App extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
 }
 
 export default App;
